d98230e601f8f210b96a5da0cb5c837b897921b6
Victor Penso <vic.penso@gmail.com>
2025-06-17 11:43:41 +0200

    use gofumt for formating...

--- go/redfish/hello-world/hello-world.go
+++ go/redfish/hello-world/hello-world.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/stmcginnis/gofish"
+	"github.com/stmcginnis/gofish"
 )
 
 func main() {
-
 	BMC_PASSWORD := os.Getenv("BMC_PASSWORD")
 	BMC_USER := os.Getenv("BMC_USER")
 	BMC_FQDN := "https://" + os.Getenv("BMC_FQDN")
@@ -19,17 +18,17 @@ func main() {
 	bmcConfig.Endpoint = BMC_FQDN
 	bmcConfig.Insecure = true
 
-    client, err := gofish.Connect(bmcConfig)
-    if err != nil {
-        panic(err)
-    }
-    defer client.Logout()
+	client, err := gofish.Connect(bmcConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Logout()
 
-    service := client.GetService()
-    fmt.Printf("%s Redfish Version %s ", bmcConfig.Endpoint, service.RedfishVersion)
-    systems, err := service.Systems()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("-- %d systems\n found", len(systems))
+	service := client.GetService()
+	fmt.Printf("%s Redfish Version %s ", bmcConfig.Endpoint, service.RedfishVersion)
+	systems, err := service.Systems()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("-- %d systems\n found", len(systems))
 }
